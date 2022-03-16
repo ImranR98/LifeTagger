6e6990a17900497fdf76cd7d9a12ab51838b8f76
Imran Remtulla <iremtulla15@gmail.com>
2022-03-16 13:09:46 -0400

    Added environment var. validation + updated TODOs

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 /*
-TODO: Everything:
-- Validate that env. vars. exist and are valid (target directory exists, number is a number, etc.)
-- Check if a file has already been tagged (if it follows the -[tag1, tag2, tag3] format)
-- Grab ten untagged files (in a random order - explicitly not chronological as this process also acts to remind the user of random memories every day)
-- Find a way to open the file in the appropriate default program (image viewer or media player) - this will probably be the toughest part
-    - Especially see if it can be opened in an unfocused window to the side, so the terminal remains in focus (or open a preview inside the terminal?)
-- Allow the user to type in tags and make sure it is a comma separated string with letters and numbers only
-    - Add a `tags.txt` to the target directory and update it when new tags are added
-    - Show the user the contents of `tags.txt` as they are typing to remind them of existing tags they may want to re-use
-- Save the users validated tags by appending to the filename in the appropriate format and adding to the exiftool `Description` tag
-    - If the exiftool addition was not possible, warn the user (this is not an error, just an FYI as filename should be enough)
-- Would prefer to scan directory and filter already tagged items on each run, but this may not be performant when working with thousands of files
-    - May have to rely on a DB to save some state; hopefully not
+TODO:
+1. Grab array of all files in the directory, filter out already tagged ones (those that follow the -[tag1, tag2, tag3].ext format)
+2. From the remaining array, pick X number of files at random (forget the rest)
+3. For each remaining file...
+    1. Open the file in the appropriate default program (image viewer or media player) - not sure how to do this yet
+        - Especially see if it can be opened in an unfocused window to the side, so the terminal remains in focus (or open a preview inside the terminal?)
+    2. Check if a tags.txt exists in the directory and if so, show its contents (preferably in a new window while also keeping terminal in focus)
+    3. Ask the user to type in tags and make sure it is a comma separated string with letters and numbers only (also trim each tag and make it all lowercase)
+    4. Save the users validated tags by appending to the filename in the appropriate format and adding to the exiftool `Description` tag
+        - If the exiftool addition was not possible, warn the user (this is not an error, just an FYI as filename should be enough)
+    5. Update tags.txt with any new tags
+
+Notes:
+1. Scanning the directory and filtering previously tagged files may be too slow to repeat on each run - may need to think about a solution
 
 Possible future upgrades:
-- A GUI to allow for built in file previews and tag autocomplete
-- A server version of this that can be hosted by a user, then accessed any time from their phone or PC; also giving them daily notification reminders
+1. A GUI to allow for built in file previews and tag autocomplete
+2. A server version of this that can be hosted by a user, then accessed any time from their phone or PC; also giving them daily notification reminders
 */
+
+// Before anything else, prepare the environment variables
+require('./prepEnv').prepEnv()
---
+++ prepEnv.js
@@ -0,0 +1,56 @@
+// Functions to parse and validate environment variables/
+// Always run prepEnv() before anything else, and update that function when/if environment variable needs change
+
+const fs = require('fs')
+require('dotenv').config({ path: 'app.env' })
+
+/** Check that the specified directory exists (if not, optionally create it) and return the path; throws an error if the dir was not found/created */
+module.exports.ensureDir = (path, createIfNotFound = false) => {
+    if (!fs.existsSync(path)) {
+        if (!createIfNotFound) throw new Error(`'${path}' does not exist`)
+        else fs.mkdirSync(path)
+    } else if (!fs.statSync(path).isDirectory())
+        throw new Error(`'${path}' is not a directory`)
+    return path
+}
+/** Converts the provided string into a positive (> 0) integer; throws an error if the string does not fully contain an int ('12AB' is only a partial int) */
+module.exports.strToPosInt = (str) => {
+    if (Number.isNaN(str))
+        throw new Error(`'${str}' is not a number`)
+    else {
+        const result = Number.parseInt(str)
+        if (result <= 0) throw new Error(`'${str}' is not a positive integer`)
+        return result
+    }
+}
+/** Returns a boolean value if the provided string is 'true', 'false', or empty (case insensitive); else throws an error  */
+module.exports.strToBool = (str) => {
+    const s = str.toLowerCase().trim()
+    const trueFalseEmptyRegex = /(^$|^(t|T)(r|R)(u|U)(e|E)$|^(f|F)(a|A)(l|L)(s|S)(e|E)$)/
+    if (!trueFalseEmptyRegex.test(s)) throw new Error(`'${str}' is neither 'true' nor 'false' nor empty`)
+    else return s === 'true'
+}
+
+/** Validate the required evironment variable strings (as defined/hardcoded in this function) and replace them with parsed versions */
+module.exports.prepEnv = () => {
+    const requiredVars = {
+        paths: {
+            vars: ['MEDIA_FOLDER_PATH'],
+            parseFunc: this.ensureDir
+        },
+        posInts: {
+            vars: ['BATCH_SIZE'],
+            parseFunc: this.strToPosInt
+        },
+        bools: {
+            vars: ['EXIF_TAGS_ONLY'],
+            parseFunc: this.strToBool
+        }
+    }
+    Object.keys(requiredVars).forEach(key => {
+        for (let i = 0; i < requiredVars[key].vars.length; i++) {
+            if (typeof process.env[requiredVars[key].vars[i]] !== 'string') throw new Error(`Environment variable '${requiredVars[key].vars[i]}' does not exist`)
+            else process.env[requiredVars[key].vars[i]] = requiredVars[key].parseFunc(process.env[requiredVars[key].vars[i]])
+        }
+    })
+}
